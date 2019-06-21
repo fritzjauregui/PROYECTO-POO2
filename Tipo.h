78a1b210ad2f640a35d71aea64ade4e9dc5cf633
utec <ian.ariasschreiber@utec.edu.pe>
2019-06-21 16:39:32 -0500

    segunda actualizacion

---
+++ Tipo.h
@@ -0,0 +1,8 @@
+//
+// Created by utec on 21/06/19.
+//
+
+#ifndef GAME_TIPO_H
+#define GAME_TIPO_H
+
+#endif //GAME_TIPO_H
---
+++ cmake-build-debug/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/utec/Documentos/PFinal/PROYECTO-POO2/Controlador.cpp" "/home/utec/Documentos/PFinal/PROYECTO-POO2/cmake-build-debug/CMakeFiles/game.dir/Controlador.cpp.o"
+  "/home/utec/Documentos/PFinal/PROYECTO-POO2/Objeto.cpp" "/home/utec/Documentos/PFinal/PROYECTO-POO2/cmake-build-debug/CMakeFiles/game.dir/Objeto.cpp.o"
+  "/home/utec/Documentos/PFinal/PROYECTO-POO2/Tierra.cpp" "/home/utec/Documentos/PFinal/PROYECTO-POO2/cmake-build-debug/CMakeFiles/game.dir/Tierra.cpp.o"
+  "/home/utec/Documentos/PFinal/PROYECTO-POO2/main.cpp" "/home/utec/Documentos/PFinal/PROYECTO-POO2/cmake-build-debug/CMakeFiles/game.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
